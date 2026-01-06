dc7e910bf17c0ade75577e736fb02cf0d94d7d75
maxpeterkaya <git@maxkaya.com>
2026-01-06 17:09:29 -0500

    init loading component

---
+++ src/components/loading.tsx
@@ -0,0 +1,9 @@
+export default function LoadingComponent() {
+    return (
+        <>
+            <h3 className={"text-gray-400 text-lg"}>
+                Currently fetching weather data...
+            </h3>
+        </>
+    )
+}
