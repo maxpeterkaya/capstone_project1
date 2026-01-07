0f182d440fd92d108c2d7a2ab2050b0cc6107e37
maxpeterkaya <git@maxkaya.com>
2026-01-07 18:48:22 -0500

    feat: change ui for current weather to equally spaced list

--- src/components/currentWeather.tsx
+++ src/components/currentWeather.tsx
@@ -1,23 +1,48 @@
 import type {Forecast} from "../types/forecast.ts";
 
-export default function CurrentWeather({data}: {data: Forecast}) {
+export default function CurrentWeather({data}: { data: Forecast }) {
     return (
-        <>
-            <h4>
-               Temperature: {data.current.temperature_2m}&deg;
-            </h4>
-            <h4>
-               Apparent Temperature: {data.current.apparent_temperature}&deg;
-            </h4>
-            <h4>
-                Precipitation: {data.current.precipitation}%
-            </h4>
-            <h4>
-                Humidity: {data.current.relative_humidity_2m}%
-            </h4>
-            <h4>
-                Rain: {data.current.rain}%
-            </h4>
-        </>
+        <div className={"w-[50vw] h-full flex flex-col gap-4"}>
+            <div className={"flex flex-row justify-between items-center"}>
+                <span>
+                    Temperature:
+                </span>
+                <span>
+                    {data.current.temperature_2m}&deg;
+                </span>
+            </div>
+            <div className={"flex flex-row justify-between"}>
+                <span>
+                    Apparent Temperature:
+                </span>
+                <span>
+                    {data.current.apparent_temperature}&deg;
+                </span>
+            </div>
+            <div className={"flex flex-row justify-between"}>
+                <span>
+                    Precipitation:
+                </span>
+                <span>
+                    {data.current.precipitation}%
+                </span>
+            </div>
+            <div className={"flex flex-row justify-between"}>
+                <span>
+                    Humidity:
+                </span>
+                <span>
+                    {data.current.relative_humidity_2m}%
+                </span>
+            </div>
+            <div className={"flex flex-row justify-between"}>
+                <span>
+                    Rain:
+                </span>
+                <span>
+                    {data.current.rain}%
+                </span>
+            </div>
+        </div>
     )
 }
