2aab187af057eb246c8606f0d28a4ef914495165
maxpeterkaya <git@maxkaya.com>
2026-01-07 15:45:40 -0500

    feat: error component wraps around children to properly make user aware of error

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -1,7 +1,14 @@
-export default function Error({error}: {error: string}) {
+import type {ReactNode} from "react";
+
+export default function Error({error, children}: {error: string, children: ReactNode}) {
     return (
         <>
-            {error}
+            <h3 className="text-xl font-bold text-red-400">
+                {error}
+            </h3>
+            <div className={`cursor-none pointer-events-none ${error.length > 0 ? "opacity-40" : ""}`}>
+                {children}
+            </div>
         </>
     )
 }
